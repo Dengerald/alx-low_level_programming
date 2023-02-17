c5429207822cc5f228c63bd4be71b6790084bde4
Dengerald <dennisagose@gmail.com>
2023-02-17 10:04:18 -0800

    Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print in alphabetic manner
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
+	return (0);
+}
